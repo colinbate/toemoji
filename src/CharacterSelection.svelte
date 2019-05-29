bbde0e97a5a48f4c2cbf8c57a267c3094f5e1d83
Colin Bate <colin@colinbate.com>
2019-05-29 01:36:39 -0300

    Improve character chooser UI

--- src/CharacterSelection.svelte
+++ src/CharacterSelection.svelte
@@ -5,10 +5,12 @@ import {createEventDispatcher} from 'svelte';
 export let mode;
 const dispatch = createEventDispatcher();
 let currentPlayer = 0;
+let taken = '';
 if (mode === '1p') {
   players.set([null, 'computer']);
 }
 function setPlayer(ev) {
+  taken = ev.detail.character;
   players.update(p => p.map((play, pi) => currentPlayer === pi ? ev.detail.character : play));
   if (mode === '1p' || currentPlayer === 1) {
     dispatch('complete');
@@ -18,4 +20,4 @@ function setPlayer(ev) {
 }
 </script>
 
-<Chooser player={currentPlayer} on:choose={setPlayer}/>
+<Chooser disable={taken} player={currentPlayer} on:choose={setPlayer}/>
--- src/Chooser.svelte
+++ src/Chooser.svelte
@@ -1,10 +1,13 @@
 <script>
 import {createEventDispatcher} from 'svelte';
 export let player;
+export let disable;
 const dispatch = createEventDispatcher();
 
 function makeChoice(character) {
-  dispatch('choose', {character});
+  if (character !== disable) {
+    dispatch('choose', {character});
+  }
 }
 
 const emojiList = [
@@ -24,14 +27,39 @@ const emojiList = [
 </script>
 <style>
 .chooser {
+  width: 100%;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-template-rows: repeat(3, 1fr);
 }
+.chooser img {
+  width: 100%;
+}
+
+.chooser div {
+  padding: 0.5rem;
+  cursor: pointer;
+}
+.chooser div.disable {
+  cursor: not-allowed;
+}
+.chooser div.disable img {
+  opacity: 0.3;
+}
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
 </style>
-<h2>Choose Player {player + 1}</h2>
-<div class="chooser">
-  {#each emojiList as em}
-  <img src="/emoji/{em}.svg" alt={em} on:click={() => makeChoice(em)}>
-  {/each}
+<div class="wrapper">
+  <h1>Choose Player {player + 1}</h1>
+  <div class="chooser">
+    {#each emojiList as em}
+    <div class:disable={em === disable}>
+    <img src="/emoji/{em}.svg" alt={em} on:click={() => makeChoice(em)}>
+    </div>
+    {/each}
+  </div>
 </div>
